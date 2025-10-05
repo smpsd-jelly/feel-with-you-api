effdbe33bd2dfe184ef4c310775a2240f7308c46
smpsd-jelly <panipaksenmee@gmail.com>
2025-10-05 14:56:08 +0700

    added default emotion pic, mood seeder, getDefaultMood resolver schema and edited bugs

--- src/resolvers/mood.js
+++ src/resolvers/mood.js
@@ -1,6 +1,8 @@
-const db = require('../models');
-const { Op } = require('sequelize');
+const db = require("../models");
+const { Op } = require("sequelize");
 const { Mood } = db;
+
+const DEFAULT_MOOD_NAME = 'default';
 
 const moodResolvers = {
   Query: {
@@ -9,22 +11,32 @@ const moodResolvers = {
         const mood = await Mood.findByPk(id);
         return mood;
       } catch (err) {
-        console.error('getMoodById error:', err);
-        throw new Error('Internal Server Error');
+        console.error("getMoodById error:", err);
+        throw new Error("Internal Server Error");
       }
     },
     getMoodByName: async (_, { name }) => {
       try {
         const mood = await Mood.findOne({
-          where: { name: { [Op.like]: name } }
+          where: { name: { [Op.like]: name } },
         });
         return mood;
       } catch (err) {
-        console.error('getMoodByName error:', err);
-        throw new Error('Internal Server Error');
+        console.error("getMoodByName error:", err);
+        throw new Error("Internal Server Error");
       }
     },
-  }
-}
+    getDefaultMood: async () => {
+      try {
+        return await Mood.findOne({
+          where: { name: { [Op.eq]: DEFAULT_MOOD_NAME } },
+        });
+      } catch (err) {
+        console.error("getDefaultMood error:", err);
+        throw new Error("Internal Server Error");
+      }
+    },
+  },
+};
 
 module.exports = moodResolvers;
